4b352bd0c72f56c3339b2c3bca3d524455cb271e
guangxush <guangxush@163.com>
2017-11-08 00:23:39 +0800

    203.RemoveLinkedListElements.java

---
+++ 203.RemoveLinkedListElements.java
@@ -0,0 +1,32 @@
+package cn.shgx.easy;
+/**
+ * Remove all elements from a linked list of integers that have value val.
+ * Example
+ * Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
+ * Return: 1 --> 2 --> 3 --> 4 --> 5
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class RemoveLinkedListElements {
+	 public ListNode removeElements(ListNode head, int val) {
+		 ListNode dummy = new ListNode(0);
+		 dummy.next = head;
+		 ListNode p = dummy;
+		 ListNode q = head;
+		 while(q!=null) {
+			 if(q.val==val) {
+				 p.next = q.next;
+			 }else {
+				 p = p.next;
+			 }
+			 q = q.next;
+		 }
+		 return dummy.next;
+	 }
+}
