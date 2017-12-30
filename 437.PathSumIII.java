6e42de0e32489908e5fffc023abb8a7a664d62f8
guangxush <guangxush@163.com>
2017-12-31 00:16:04 +0800

    437.PathSumIII.java

--- 437.PathSumIII.java
+++ 437.PathSumIII.java
@@ -19,5 +19,21 @@ package cn.shgx.easy;
    3. -3 -> 11
  */
 public class PathSumIII {
-
+    public int pathSum(TreeNode root, int sum) {
+        if(root==null) {
+        	return 0;
+        }
+        return dfs(root, sum)+pathSum(root.left, sum)+pathSum(root.right, sum);
+    }
+    public int dfs(TreeNode root,int sum) {
+    	int res = 0;
+    	if(root==null) {
+    		return res;
+    	}
+    	if(sum==root.val)
+    		res++;
+    	res+=dfs(root.left, sum-root.val);
+    	res+=dfs(root.right, sum-root.val);
+    	return res;
+    }
 }
