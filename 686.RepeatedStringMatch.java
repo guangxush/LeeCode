128cb0a497b854678413759a4ed51247ae67c1cb
guangxush <guangxush@163.com>
2018-04-09 23:37:34 +0800

    ‘686.RepeatedStringMatch.java git commit -m ‘686.RepeatedStringMatch.java

---
+++ 686.RepeatedStringMatch.java
@@ -0,0 +1,20 @@
+package cn.shgx.easy;
+/**
+ * Given two strings A and B, find the minimum number of times A has to be repeated such that B is a substring of it. 
+ * If no such solution, return -1.
+ * For example, with A = "abcd" and B = "cdabcdab".
+ * Return 3, because by repeating A three times (“abcdabcdabcd”), B is a substring of it; 
+ * and B is not a substring of A repeated two times ("abcdabcd").
+ * Note:
+ * The length of A and B will be between 1 and 10000.
+ */
+public class RepeatedStringMatch {
+    public int repeatedStringMatch(String A, String B) {
+        String as = A;
+        for(int rep=1;rep<=B.length()/A.length()+2;rep++,as+=A) {
+        	if(as.indexOf(B)!=-1)
+        		return rep;
+        }
+        return -1;
+    }
+}
