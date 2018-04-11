d7a2d2bdc771fa2a270f4d1cbeb3064dfb651b46
guangxush <guangxush@163.com>
2018-04-11 12:10:50 +0800

    687.LongestUnivaluePath.java

--- 687.LongestUnivaluePath.java
+++ 687.LongestUnivaluePath.java
@@ -42,4 +42,23 @@ public class LongestUnivaluePath {
     	if(val==node.val)return Math.max(left, right)+1;
     	return 0;
     }
+    private int res = 0;
+    public int longestUnivaluePath1(TreeNode root) {
+        calculate(root);
+        return res;
+    }
+    private int calculate(TreeNode root) {
+    	if(root==null) return 0;
+    	int leftCount = calculate(root.left);
+    	int rightCount = calculate(root.right);
+    	int left = 0, right =0;
+    	if(root.left!=null&&root.left.val==root.val) {
+    		left = leftCount+1;
+    	}
+    	if(root.right!=null&&root.right.val==root.val) {
+    		right = rightCount+1;
+    	}
+    	res = Math.max(res, left+right);
+    	return Math.max(left, right);
+    }
 }
