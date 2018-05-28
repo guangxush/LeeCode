a5840070156dccd0ebef95de46458d8dc639d5d3
guangxush <guangxush@163.com>
2018-05-28 22:52:59 +0800

    103.BinaryTreeZigzagLevelOrderTraversal.java

---
+++ 103.BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,62 @@
+package cn.shgx.medium;
+import java.util.*;
+/**
+Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its zigzag level order traversal as:
+[
+  [3],
+  [20,9],
+  [15,7]
+]
+ */
+public class BinaryTreeZigzagLevelOrderTraversal {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> sol = new ArrayList<>();
+        travel(root,sol,0);
+        return sol;
+    }
+    private void travel(TreeNode curr,List<List<Integer>> sol,int level) {
+    	if(curr==null) return;
+    	if(sol.size()<=level) {
+    		List<Integer> newLevel = new LinkedList<>();
+    		sol.add(newLevel);
+    	}
+    	List<Integer> collection = sol.get(level);
+    	if(level%2==0) collection.add(curr.val);
+    	else collection.add(0,curr.val);
+    	travel(curr.left,sol,level+1);
+    	travel(curr.right,sol,level+1);
+    }
+    public List<List<Integer>> zigzagLevelOrder1(TreeNode root) {
+    	List<List<Integer>> res = new ArrayList<>();
+    	if(root == null) return res;
+    	Queue<TreeNode> q = new LinkedList<>();
+	    q.add(root);
+	    boolean order = true;
+	    int size = 1;
+	    while(!q.isEmpty()) {
+	        List<Integer> tmp = new ArrayList<>();
+	        for(int i = 0; i < size; ++i) {
+	            TreeNode n = q.poll();
+	            if(order) {
+	                tmp.add(n.val);
+	            } else {
+	                tmp.add(0, n.val);
+	            }
+	            if(n.left != null) q.add(n.left);
+	            if(n.right != null) q.add(n.right);
+	        }
+	        res.add(tmp);
+	        size = q.size();
+	        order = order ? false : true;
+	    }
+	    return res;
+    }
+}
